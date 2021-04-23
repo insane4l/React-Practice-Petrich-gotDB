de551b2d82cdad0e23e3ffcebd359c0fd81a8587
insane4l <43768644+insane4l@users.noreply.github.com>
2021-04-23 18:48:43 +0300

    API base scripts added

--- src/components/charDetails/charDetails.js
+++ src/components/charDetails/charDetails.js
@@ -1,30 +1,46 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
 import './charDetails.css';
+
+const DetailsBlock = styled.div`
+    background-color: #fff;
+    padding: 25px 25px 15px 25px;
+    margin-bottom: 40px;
+    h4 {
+        margin-bottom: 20px;
+        text-align: center;
+    }
+`;
+
+const DescrTitle = styled.span`
+    font-weight: bold;
+`;
+
 export default class CharDetails extends Component {
 
     render() {
         return (
-            <div className="char-details rounded">
+            <DetailsBlock className="rounded">
                 <h4>John Snow</h4>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender</span>
+                        <DescrTitle>Gender</DescrTitle>
                         <span>male</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born</span>
+                        <DescrTitle>Born</DescrTitle>
                         <span>1783</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died</span>
+                        <DescrTitle>Died</DescrTitle>
                         <span>1820</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture</span>
+                        <DescrTitle>Culture</DescrTitle>
                         <span>First</span>
                     </li>
                 </ul>
-            </div>
+            </DetailsBlock>
         );
     }
 }
--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,32 +1,45 @@
 import React, {Component} from 'react';
-import './randomChar.css';
+import styled from 'styled-components';
 
+const RandomBlock = styled.div`
+    background-color: #fff;
+    padding: 25px 25px 15px 25px;
+    margin-bottom: 40px;
+    h4 {
+        margin-bottom: 20px;
+        text-align: center;
+    }
+`;
+
+const DescrTitle = styled.span`
+    font-weight: bold;
+`;
 export default class RandomChar extends Component {
 
     render() {
 
         return (
-            <div className="random-block rounded">
+            <RandomBlock className="rounded">
                 <h4>Random Character: John</h4>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender </span>
+                        <DescrTitle>Gender </DescrTitle>
                         <span>male</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born </span>
+                        <DescrTitle>Born </DescrTitle>
                         <span>11.03.1039</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died </span>
+                        <DescrTitle>Died </DescrTitle>
                         <span>13.09.1089</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture </span>
+                        <DescrTitle>Culture </DescrTitle>
                         <span>Anarchy</span>
                     </li>
                 </ul>
-            </div>
+            </RandomBlock>
         );
     }
 }
