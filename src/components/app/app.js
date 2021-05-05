0bbdcbd9d67777c554e8ec0a3e0cbfd66aa065e7
insane4l <43768644+insane4l@users.noreply.github.com>
2021-05-05 21:12:01 +0300

    Router and Links added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import './app.css';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import {CharactersPage, HousesPage, BooksPage} from '../pages/';
 import ErrorMessage from '../errorMessage';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 
 export default class App extends Component {
@@ -34,23 +36,27 @@ export default class App extends Component {
         }
 
         return (
-            <> 
-                <Container>
-                    <Header />
-                </Container>
-                <Container>
-                    <Row>
-                        <Col lg={{size: 5, offset: 0}}>
-                            {char}
-                            <button
-                            onClick={this.toggleRandomChar}>Toggle Random Character</button>
-                        </Col>
-                    </Row>
-                    <CharactersPage/>
-                    <BooksPage/>
-                    <HousesPage/>
-                </Container>
-            </>
+            <Router>
+                <div className='app'> 
+                    <Container>
+                        <Header />
+                    </Container>
+                    <Container>
+                        <Row>
+                            <Col lg={{size: 5, offset: 0}}>
+                                {char}
+                                <button
+                                onClick={this.toggleRandomChar}>Toggle Random Character</button>
+                            </Col>
+                        </Row>
+
+                        <Route path='/characters' exact component={CharactersPage} />
+                        <Route path='/books' exact component={BooksPage} />
+                        <Route path='/houses' exact component={HousesPage} />
+                        
+                    </Container>
+                </div>
+            </Router>
         );
     }
 };
